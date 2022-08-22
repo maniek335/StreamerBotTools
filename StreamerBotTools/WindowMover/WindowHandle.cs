7821d6672eb698028da27647770852ea3e7af3ad
Kubisz Mariusz <kontakt@kubiszmariusz.pl>
2022-08-22 14:37:20 +0200

    Resize and move the window

--- StreamerBotTools/WindowMover/WindowHandle.cs
+++ StreamerBotTools/WindowMover/WindowHandle.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Text;
 using StreamerBotTools.Internal;
 
@@ -28,6 +29,65 @@ namespace StreamerBotTools.WindowMover
 
                 return String.Empty;
             }
+        }
+
+        public Rectangle GetWindowRectangle()
+        {
+            User32.GetWindowRect(hWnd, out RECT rect);
+
+            return Rectangle.FromLTRB(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
+
+        public bool SetWindowPos(Rectangle rect)
+        {
+            return User32.SetWindowPos(
+                hWnd: hWnd,
+                hWndInsertAfter: IntPtr.Zero,
+                X: rect.X,
+                Y: rect.Y,
+                cx: rect.Width,
+                cy: rect.Height,
+                uFlags: 0);
+        }
+
+        public bool Resize(int widthOffset, int heightOffset)
+        {
+            var rect = GetWindowRectangle();
+
+            rect.Width += widthOffset;
+            rect.Height += heightOffset;
+
+            return SetWindowPos(rect);
+        }
+
+        public bool ResizeTo(int newWidth, int newHeight)
+        {
+            var rect = GetWindowRectangle();
+
+            rect.Width = newWidth;
+            rect.Height = newHeight;
+
+            return SetWindowPos(rect);
+        }
+
+        public bool Move(int xOffset, int yOffset)
+        {
+            var rect = GetWindowRectangle();
+
+            rect.X += xOffset;
+            rect.Y += yOffset;
+
+            return SetWindowPos(rect);
+        }
+
+        public bool MoveTo(int newX, int newY)
+        {
+            var rect = GetWindowRectangle();
+
+            rect.X = newX;
+            rect.Y = newY;
+
+            return SetWindowPos(rect);
         }
     }
 }
--- docs/src/api/WindowMover/WindowHandle.md
+++ docs/src/api/WindowMover/WindowHandle.md
@@ -41,3 +41,85 @@
 - **See also:**
   - [API - WindowFinder.FindWindowsByTitle()](./WindowFinder#findwindowsbytitle)
   - [API - WindowFinder.FindWindowByTitle()](./WindowFinder#findwindowbytitle)
+
+## GetWindowRectangle()
+
+- **Type**
+
+  ```csharp
+  public class WindowHandle {
+    public System.Drawing.Rectangle GetWindowRectangle();
+  }
+  ```
+
+- **See also:**
+  - [Microsoft Docs - Rectangle](https://docs.microsoft.com/en-us/dotnet/api/system.drawing.rectangle)
+
+## SetWindowPos()
+
+- **Type**
+
+  ```csharp
+  public class WindowHandle {
+    public bool SetWindowPos(System.Drawing.Rectangle rect);
+  }
+  ```
+
+- **See also:**
+  - [Microsoft Docs - Rectangle](https://docs.microsoft.com/en-us/dotnet/api/system.drawing.rectangle)
+
+## Resize()
+
+- **Type**
+
+  ```csharp
+  public class WindowHandle {
+    public bool Resize(int widthOffset, int heightOffset);
+  }
+  ```
+
+- **Details**
+
+  Resizes the window relative to its current size.
+
+## ResizeTo()
+
+- **Type**
+
+  ```csharp
+  public class WindowHandle {
+    public bool ResizeTo(int newWidth, int newHeight);
+  }
+  ```
+
+- **Details**
+
+  Resizes the window to a new width and height.
+
+## Move()
+
+- **Type**
+
+  ```csharp
+  public class WindowHandle {
+    public bool Move(int xOffset, int yOffset);
+  }
+  ```
+
+- **Details**
+
+  Moves the window relative to its current position.
+
+## MoveTo()
+
+- **Type**
+
+  ```csharp
+  public class WindowHandle {
+    public bool MoveTo(int newX, int newY);
+  }
+  ```
+
+- **Details**
+
+  Moves the window to new coordinates on the screen.
