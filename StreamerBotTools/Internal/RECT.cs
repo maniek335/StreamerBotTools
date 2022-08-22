aad0e4e13b38765f947a11cf47984acccd0204ba
Kubisz Mariusz <kontakt@kubiszmariusz.pl>
2022-08-22 14:37:20 +0200

    Resize and move the window

---
+++ StreamerBotTools/Internal/RECT.cs
@@ -0,0 +1,7 @@
+namespace StreamerBotTools.Internal
+{
+    internal struct RECT
+    {
+        public int Left, Top, Right, Bottom;
+    }
+}
--- StreamerBotTools/Internal/User32.cs
+++ StreamerBotTools/Internal/User32.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -11,10 +11,16 @@ namespace StreamerBotTools.Internal
         [DllImport("user32.dll")]
         internal static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
 
+        [DllImport("user32.dll")]
+        internal static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
         [DllImport("user32.dll", CharSet = CharSet.Unicode)]
         internal static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
 
         [DllImport("user32.dll", CharSet = CharSet.Unicode)]
         internal static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
     }
 }
